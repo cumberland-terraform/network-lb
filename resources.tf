2901fea6a269d17d9ee15f4d330e0b69487d1c07
grant <grant.moore@maryland.gov>
2024-08-04 13:22:39 -0400

    formatting

--- resources.tf
+++ resources.tf
@@ -12,15 +12,15 @@ resource "aws_lb_listener" "this" {
     for_each                        = { for index, lb in var.lb.listeners: 
                                         index => lb }
 
-    load_balancer_arn                 = aws_lb.this.arn
-    port                              = each.value.port
-    protocol                          = each.value.protocol
-    ssl_policy                        = local.platform_defaults.listener.ssl_policy
-    certificate_arn                   = each.value.certificate_arn
+    load_balancer_arn               = aws_lb.this.arn
+    port                            = each.value.port
+    protocol                        = each.value.protocol
+    ssl_policy                      = local.platform_defaults.listener.ssl_policy
+    certificate_arn                 = each.value.certificate_arn
 
     default_action {
-        type                            = each.value.default_action.type
-        target_group_arn                = each.value.default_action.target_group_arn
+        type                        = each.value.default_action.type
+        target_group_arn            = each.value.default_action.target_group_arn
     }
 }
 
@@ -45,7 +45,7 @@ resource "aws_lb_listener_rule" "this" {
     # TODO: parameterize this block
     condition {
         host_header {
-            values = ["*"]
+            values                  = ["*"]
         }
     }
 }
