301ea0142970de39b8398380701a7acf1bb920ff
grant <grant.moore@maryland.gov>
2024-08-05 11:01:47 -0400

    set default subnet to private

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "platform" {
     domain                  = string
     availability_zones      = list(string)
     pca                     = string
-    subnet_type             = optional(string, "NETWORK ADDRESS TRANSLATION")
+    subnet_type             = optional(string, "PRIVATE")
   })
 }
 
