27378d530ab1bcfd66be2cb62828d2d793ef88cd
grant <grant.moore@maryland.gov>
2024-09-24 18:38:08 -0400

    remove depth

--- modules.tf
+++ modules.tf
@@ -1,5 +1,5 @@
 module "platform" {
-  source                = "git::ssh://git@source.mdthink.maryland.gov:22/etm/mdt-eter-platform.git?ref=v1.0.13&depth=1"
+  source                = "git::ssh://git@source.mdthink.maryland.gov:22/etm/mdt-eter-platform.git?ref=v1.0.15"
 
   platform              = local.platform
   hydration             = {
