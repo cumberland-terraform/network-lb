e30dee43d51db0713d03784fd0b1434f0b3fc30f
grant <grant.moore@maryland.gov>
2024-10-03 14:44:11 -0400

    update policy resources

--- data.tf
+++ data.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "log_access_policy" {
     actions                 = [ "s3:PutObject" ]
     resources               = [  
                                 "arn:aws:s3:::${local.bucket_name}",
+                                "arn:aws:s3:::${local.bucket_name}/*",
                                 "arn:aws:s3:::${local.bucket_name}/access/AWSLogs/${module.platform.aws.account_id}/*",
                                 "arn:aws:s3:::${local.bucket_name}/connection/AWSLogs/${module.platform.aws.account_id}/*"
                             ]
