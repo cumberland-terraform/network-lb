3df5bda184bb84e6830e0ceda762c70bfb1014be
grant <grant.moore@maryland.gov>
2024-11-01 04:25:52 -0400

    reignore

--- resources.tf
+++ resources.tf
@@ -1,6 +1,6 @@
 resource "aws_lb" "this" {
     lifecycle {
-      ignore_changes                = [ tags ]
+      ignore_changes                = [ tags, name ]
     }
 
     enable_deletion_protection      = local.platform_defaults.lb.enable_deletion_protection
